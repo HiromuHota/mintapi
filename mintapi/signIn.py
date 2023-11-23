295bfa9d51f92e7cbb7e724687c1b6987d27750e
Hiromu Hota <hiromu.hota@gmail.com>
2023-11-23 10:25:02 -0800

    Get headless working

--- mintapi/signIn.py
+++ mintapi/signIn.py
@@ -194,7 +194,7 @@ def _create_web_driver_at_mint_com(
         chrome_options.add_argument("no-sandbox")
         chrome_options.add_argument("disable-dev-shm-usage")
         chrome_options.add_argument("disable-gpu")
-        # chrome_options.add_argument("--window-size=1920x1080")
+        chrome_options.add_argument("--window-size=1280x720")
     if session_path is not None:
         chrome_options.add_argument("user-data-dir=%s" % session_path)
 
